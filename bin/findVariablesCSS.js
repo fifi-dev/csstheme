464c57424f2d0223d2a55f1f0a3ac2ae4f84fe71
Karen160 <karen.azoulay@edu.devinci.fr>
2023-12-08 11:41:34 +0100

    CT-50 : Delete variable if exist in file

---
+++ bin/findVariablesCSS.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+import path from "path";
+
+// Function to recursively search for a file in a directory
+const findFileInDirectory = (directory, fileName) => {
+    const files = fs.readdirSync(directory);
+
+    for (const file of files) {
+        const filePath = path.join(directory, file);
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+            const result = findFileInDirectory(filePath, fileName);
+            if (result) {
+                return result;
+            }
+        } else if (file === fileName) {
+            return filePath;
+        }
+    }
+
+    return null;
+};
+
+// Function to find variables.css in the project
+export const findVariablesCSS = () => {
+    // Assuming this script is in the root of your project
+    const projectRoot = process.cwd();  // Get the current working directory
+    return findFileInDirectory(projectRoot, "variables.css");
+};
